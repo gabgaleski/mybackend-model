d9682560e8e2cfe4493ddcb404e087c315558279
Gabriel Galeski <galeski35@gmail.com>
2023-10-16 15:53:32 -0300

    fix: corrige erro de cors na requisição

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,24 @@ class App {
     this.app = express();
     this.config();
     this.routes();
+
+    this.app.get('/', (req, res) => res.json({ ok: true }));
   }
 
   private routes(): void {
     this.app.use(routes);
   }
 
-  private config(): void {
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
     this.app.use(express.json());
+    this.app.use(accessControl);
   }
 
   public start(PORT: string | number): void {
